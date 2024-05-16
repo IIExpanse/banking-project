b007ca1977d8aa9eb2c7fb5b708cfd0056ee88f6
Alexander Bulatov <abulatov@nota.tech>
2024-05-16 15:18:03 +0300

    Add more dependencies for data layer, lombok and mapstruct

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -16,11 +16,17 @@ dependencies {
     implementation(platform("io.quarkus.platform:quarkus-bom:" + properties["quarkusPlatformVersion"]))
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-liquibase")
+    implementation("io.quarkus:quarkus-jdbc-postgresql")
     implementation("io.quarkus:quarkus-reactive-pg-client")
+    implementation("io.quarkus:quarkus-hibernate-reactive-panache")
+    implementation("org.mapstruct:mapstruct:" + properties["mapstructVersion"])
+
+    compileOnly("org.projectlombok:lombok:" + properties["lombokVersion"])
 
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.quarkus:quarkus-junit5-mockito")
 }
+
 
 tasks.test {
     useJUnitPlatform()
