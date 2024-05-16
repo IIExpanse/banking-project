8f039e57abb240cc257b512b89a4a245ddf9bec0
Alexander Bulatov <abulatov@nota.tech>
2024-05-16 15:19:09 +0300

    Add entity class for User

---
+++ user-service/src/main/java/ru/expanse/entity/User.java
@@ -0,0 +1,27 @@
+package ru.expanse.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    @Column(name = "account_id", nullable = false)
+    private UUID accountId;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+}
