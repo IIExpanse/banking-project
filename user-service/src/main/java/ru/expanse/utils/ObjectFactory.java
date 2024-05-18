9d851ff03a28f8f8119d29e128bc14278879719c
Alexander Bulatov <aleks40k@yandex.ru>
2024-05-18 20:40:30 +0300

    Add util class for objects generation

---
+++ user-service/src/main/java/ru/expanse/utils/ObjectFactory.java
@@ -0,0 +1,13 @@
+package ru.expanse.utils;
+
+import ru.expanse.entity.User;
+
+public class ObjectFactory {
+    public static User getDefaultUser() {
+        return User.builder()
+                .name("John Doe")
+                .email("john.doe@example.com")
+                .phoneNumber("+79001234567")
+                .build();
+    }
+}
