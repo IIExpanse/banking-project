60f6373191e5c13aa6368bf5f191dc2734d30714
Alexander Bulatov <abulatov@nota.tech>
2024-05-21 19:40:15 +0300

    Add UserService interface

---
+++ user-service/src/main/java/ru/expanse/service/UserService.java
@@ -0,0 +1,16 @@
+package ru.expanse.service;
+
+import io.smallrye.mutiny.Uni;
+import ru.expanse.dto.UserDto;
+
+import java.util.UUID;
+
+public interface UserService {
+    Uni<UserDto> addUser(UserDto userDto);
+
+    Uni<UserDto> getUserById(UUID id);
+
+    Uni<UserDto> updateUser(UserDto userDto);
+
+    Uni<Boolean> deleteUser(long id);
+}
