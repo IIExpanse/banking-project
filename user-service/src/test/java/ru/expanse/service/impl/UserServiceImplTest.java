c6dfc622a35f64cc2f661c9d675944976a47b755
Alexander Bulatov <abulatov@nota.tech>
2024-05-23 19:07:09 +0300

    Add class for UserService tests

---
+++ user-service/src/test/java/ru/expanse/service/impl/UserServiceImplTest.java
@@ -0,0 +1,24 @@
+package ru.expanse.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
