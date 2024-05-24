95734596194f6dd28de86860bc011ccf139fa8f2
Alexander Bulatov <abulatov@nota.tech>
2024-05-24 19:19:52 +0300

    Add unit test for adding new user

--- user-service/src/test/java/ru/expanse/service/impl/UserServiceImplTest.java
+++ user-service/src/test/java/ru/expanse/service/impl/UserServiceImplTest.java
@@ -1,13 +1,43 @@
 package ru.expanse.service.impl;
 
+import io.smallrye.mutiny.Uni;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import ru.expanse.entity.User;
+import ru.expanse.mapper.UserMapper;
+import ru.expanse.repository.UserRepository;
+import ru.expanse.utils.ObjectFactory;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceImplTest {
+    UserRepository userRepository;
+    UserServiceImpl userService;
+    UserMapper userMapper;
+
+    @BeforeEach
+    void refresh() {
+        userRepository = Mockito.mock(UserRepository.class);
+        userMapper = Mappers.getMapper(UserMapper.class);
+        userService = new UserServiceImpl(userRepository, userMapper);
+    }
 
     @Test
-    void addUser() {
+    void addUser() throws Exception {
+        User passedUser = ObjectFactory.getDefaultUser();
+        User returnedUser = ObjectFactory.getDefaultUser();
+        returnedUser.setId(UUID.randomUUID());
+
+        Mockito.when(userRepository.persist(ArgumentMatchers.any(User.class)))
+                .thenReturn(Uni.createFrom().item(returnedUser));
+
+        UUID id = userService.addUser(userMapper.mapToDto(passedUser)).subscribe().asCompletionStage().get().id();
+        assertEquals(returnedUser.getId(), id);
     }
 
     @Test
