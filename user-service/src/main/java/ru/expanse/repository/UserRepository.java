10b8cc36db5a7382934d91c32c2a923cc73da307
Alexander Bulatov <abulatov@nota.tech>
2024-05-29 18:47:44 +0300

    feat: expand filter method by birthDate search, rename to getFilteredUsers()

--- user-service/src/main/java/ru/expanse/repository/UserRepository.java
+++ user-service/src/main/java/ru/expanse/repository/UserRepository.java
@@ -7,19 +7,32 @@ import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import ru.expanse.entity.User;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
 @ApplicationScoped
 public class UserRepository implements PanacheRepositoryBase<User, UUID> {
 
-    public Uni<List<User>> filterUsersByNameAndEmail(String name, String email) {
+    public Uni<List<User>> getFilteredUsers(
+            String name,
+            String email,
+            Timestamp startBirthDate,
+            Timestamp endBirthDate) {
         PanacheQuery<User> query = this.findAll();
         if (name != null) {
             query = query.filter("nameFilter", Parameters.with("searchName", name));
         }
         if (email != null) {
             query = query.filter("emailFilter", Parameters.with("searchEmail", email));
+        }
+        if (startBirthDate != null || endBirthDate != null) {
+            startBirthDate = startBirthDate == null ? Timestamp.from(Instant.MIN) : startBirthDate;
+            endBirthDate = endBirthDate == null ? Timestamp.from(Instant.MAX) : endBirthDate;
+
+            query = query.filter("birthDateFilter",
+                    Parameters.with("startBirthDate", startBirthDate).and("endBirthDate", endBirthDate));
         }
         return query.list();
     }
