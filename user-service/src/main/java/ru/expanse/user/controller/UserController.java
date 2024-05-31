81842621f3c54147640bbfe192a4906b05711c0f
Alexander Bulatov <abulatov@nota.tech>
2024-05-31 19:41:23 +0300

    feat: add gRPC controller

---
+++ user-service/src/main/java/ru/expanse/user/controller/UserController.java
@@ -0,0 +1,69 @@
+package ru.expanse.user.controller;
+
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Uni;
+import lombok.RequiredArgsConstructor;
+import ru.expanse.user.proto.GrpcUserController;
+import ru.expanse.user.proto.UserDto;
+import ru.expanse.user.proto.UserFilterRequest;
+import ru.expanse.user.proto.UserRequest;
+import ru.expanse.user.proto.UserResponse;
+import ru.expanse.user.service.UserService;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.UUID;
+
+@GrpcService
+@RequiredArgsConstructor
+public class UserController implements GrpcUserController {
+    private final UserService userService;
+
+    @Override
+    public Uni<UserResponse> addUser(UserDto request) {
+        return userService.addUser(request)
+                .map(this::packDto);
+    }
+
+    @Override
+    public Uni<UserResponse> getUserById(UserRequest request) {
+        return userService.getUserById(UUID.fromString(request.getId()))
+                .map(this::packDto);
+    }
+
+    @Override
+    public Uni<UserResponse> getFilteredUsers(UserFilterRequest request) {
+        return userService.getFilteredUsers(
+                        request.getName(),
+                        request.getEmail(),
+                        new Timestamp(request.getStartBirthDate()),
+                        new Timestamp(request.getEndBirthDate()))
+                .map(this::packDto);
+    }
+
+    @Override
+    public Uni<UserResponse> updateUser(UserDto request) {
+        return userService.updateUser(request)
+                .map(this::packDto);
+    }
+
+    @Override
+    public Uni<UserResponse> deleteUser(UserRequest request) {
+        return userService.deleteUser(UUID.fromString(request.getId()))
+                .map(isDeleted -> UserResponse.newBuilder()
+                        .setIsFound(isDeleted)
+                        .build());
+    }
+
+    private UserResponse packDto(UserDto dto) {
+        return UserResponse.newBuilder()
+                .addAllUsers(List.of(dto))
+                .build();
+    }
+
+    private UserResponse packDto(List<UserDto> list) {
+        return UserResponse.newBuilder()
+                .addAllUsers(list)
+                .build();
+    }
+}
