504b13f0627e30af5c5e66382e5ced9f4a0cff72
Alexander Bulatov <abulatov@nota.tech>
2024-05-31 19:41:07 +0300

    feat: add mapping configure for conversion between timestamp and time milliseconds

--- user-service/src/main/java/ru/expanse/user/mapper/UserMapper.java
+++ user-service/src/main/java/ru/expanse/user/mapper/UserMapper.java
@@ -2,10 +2,14 @@ package ru.expanse.user.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
-import ru.expanse.user.dto.UserDto;
+import org.mapstruct.ReportingPolicy;
 import ru.expanse.user.entity.User;
+import ru.expanse.user.proto.UserDto;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.CDI)
+@Mapper(componentModel = MappingConstants.ComponentModel.CDI,
+        uses = TimeStampMapper.class,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface UserMapper {
     UserDto mapToDto(User user);
 
