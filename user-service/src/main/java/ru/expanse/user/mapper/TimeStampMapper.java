3eeabff20e7671735bf8e1a080de0a2b05f8c37e
Alexander Bulatov <abulatov@nota.tech>
2024-06-01 22:45:32 +0300

    feat: configure TimeStampMapper

--- user-service/src/main/java/ru/expanse/user/mapper/TimeStampMapper.java
+++ user-service/src/main/java/ru/expanse/user/mapper/TimeStampMapper.java
@@ -1,8 +1,12 @@
 package ru.expanse.user.mapper;
 
 
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
 import java.sql.Timestamp;
 
+@Mapper(componentModel = MappingConstants.ComponentModel.CDI)
 public class TimeStampMapper {
     public long convertToMillis(Timestamp timestamp) {
         return timestamp.getTime();
