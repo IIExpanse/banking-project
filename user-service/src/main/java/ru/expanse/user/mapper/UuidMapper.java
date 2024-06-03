bb3a40bb1363d5d0bfd28c47d3b5f0c5059264ca
Alexander Bulatov <abulatov@nota.tech>
2024-06-03 23:39:24 +0300

    fix: add options for passing empty/null values

--- user-service/src/main/java/ru/expanse/user/mapper/UuidMapper.java
+++ user-service/src/main/java/ru/expanse/user/mapper/UuidMapper.java
@@ -7,11 +7,11 @@ import java.util.UUID;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.CDI)
 public class UuidMapper {
-    public UUID mapToUuid(String uuid) {
-        return UUID.fromString(uuid);
+    public UUID mapToUuid(String uuidString) {
+        return uuidString.isEmpty() ? null : UUID.fromString(uuidString);
     }
 
     public String mapToString(UUID uuid) {
-        return uuid.toString();
+        return uuid == null ? "" : uuid.toString();
     }
 }
