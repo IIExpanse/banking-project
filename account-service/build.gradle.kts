eb4e6eb1e1fc2457e59dd2db1994e9b2f2fb56c3
Alexander Bulatov <aleks40k@yandex.ru>
2024-06-09 17:34:10 +0300

    feat: add initial dependencies

--- account-service/build.gradle.kts
+++ account-service/build.gradle.kts
@@ -11,8 +11,23 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(platform("io.quarkus.platform:quarkus-bom:" + properties["quarkusPlatformVersion"]))
+    implementation("io.quarkus:quarkus-arc")
+    implementation("io.quarkus:quarkus-liquibase")
+    implementation("io.quarkus:quarkus-jdbc-postgresql")
+    implementation("io.quarkus:quarkus-reactive-pg-client")
+    implementation("io.quarkus:quarkus-hibernate-reactive-panache")
+    implementation("io.quarkus:quarkus-grpc")
+    implementation("org.mapstruct:mapstruct:" + properties["mapstructVersion"])
+    implementation("org.projectlombok:lombok-mapstruct-binding:" + properties["lombokMapstructBindingVersion"])
+
+    compileOnly("org.projectlombok:lombok:" + properties["lombokVersion"])
+    annotationProcessor("org.projectlombok:lombok:" + properties["lombokVersion"])
+    annotationProcessor("org.mapstruct:mapstruct-processor:" + properties["mapstructVersion"])
+
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("io.quarkus:quarkus-junit5-mockito")
+    testCompileOnly("org.projectlombok:lombok:" + properties["lombokVersion"])
 }
 
 tasks.test {
